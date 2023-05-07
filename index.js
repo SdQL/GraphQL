c4570dff4b01e39c5717f8a86dde30cc70ae99ad
SamyDc <sadavid2007@gmail.com>
2023-05-07 14:21:28 -0500

    Mutaciones con GraphQL

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {ApolloServer, gql} from "apollo-server"
+import {v1 as uuid} from 'uuid'
 const persons = [
     {
         name: "Maria",
@@ -40,6 +41,27 @@ const typeDefinitions = gql`
         getAllPersons: [Person]!
         getPhoneByName(name: String!): Person
     }
+    
+    type Mutation {
+        addPerson(
+            name: String!
+            phone: String
+            street: String!
+            city: String!
+        ): Person
+        
+        deletePerson(
+            name: String!
+        ): Person
+        
+        updatePerson(
+            id: ID!
+            name: String!
+            phone: String
+            street: String!
+            city: String!
+        ): Person
+    }
 `;
 
 const resolvers = {
@@ -51,6 +73,43 @@ const resolvers = {
             return persons.find(person => person.name === name)
         }
     },
+
+    Mutation: {
+        addPerson: (root, args) => {
+            const person = {...args, id: uuid()}
+            persons.push(person)
+            return person
+        },
+
+        deletePerson: (root, args) => {
+            const { name } = args;
+            const index = persons.findIndex(person => person.name === name);
+            if (index !== -1) {
+                const deletedPerson = persons.splice(index, 1)[0];
+                return deletedPerson;
+            }
+        },
+
+        updatePerson: (parent, args) => {
+            const { id, name, phone, street, city } = args;
+            const index = persons.findIndex(person => person.id === id);
+            if (index === -1) {
+                throw new Error(`No se pudo encontrar a la persona con el ID ${id}`);
+            }
+            const updatedPerson = {
+                ...persons[index],
+                name: name !== undefined ? name : persons[index].name,
+                phone: phone !== undefined ? phone : persons[index].phone,
+                street: street !== undefined ? street : persons[index].street,
+                city: city !== undefined ? city : persons[index].city
+            };
+            persons[index] = updatedPerson;
+            return updatedPerson;
+        }
+
+
+    },
+
     Person: {
         address: (root) => {
             return {
